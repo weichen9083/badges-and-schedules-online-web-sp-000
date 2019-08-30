b8b1235a451b90792e5681f2dbc3246fa6cc572d
wei chen <weichen9083@gmail.com>
2019-08-30 01:02:31 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+  x=[]
+  array.each do |name| 
+    x<<badge_maker(name)
+  end 
+  x 
+end
+
+def assign_rooms(array)
+room = 0 
+x = []
+array.each do |name|
+x<<"Hello, #{name}! You'll be assigned to room #{room+1}!"
+room +=1 
+end 
+x
+end 
+
+def printer(array)
+batch_badge_creator(array).each do |name|
+puts "#{name}" 
+end 
+assign_rooms(array).each do |name|
+puts "#{name}"
+end 
+end 
+
+
+
+
+
+
