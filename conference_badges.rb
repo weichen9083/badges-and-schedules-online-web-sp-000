a4aaa2ec7594ea27ef0f15fb3bffe9815d3181bf
wei chen <weichen9083@gmail.com>
2020-08-15 02:15:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,36 +1,9 @@
 # Write your code here.
 
-def badge_maker(name)
-  "Hello, my name is #{name}."
-end 
 
-def batch_badge_creator(array)
-  x=[]
-  array.each do |name| 
-    x<<badge_maker(name)
-  end 
-  x 
-end
-
-def assign_rooms(array)
-room = 0 
-x = []
-array.each do |name|
-x<<"Hello, #{name}! You'll be assigned to room #{room+1}!"
-room +=1 
+def badge_maker
+  
 end 
-x
-end 
-
-def printer(array)
-batch_badge_creator(array).each do |name|
-puts "#{name}" 
-end 
-assign_rooms(array).each do |name|
-puts "#{name}"
-end 
-end 
-
 
 
 
