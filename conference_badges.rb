89ffacef79846f7210dc5f8255822d8fa25ced2e
wei chen <weichen9083@gmail.com>
2020-08-15 02:33:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ end
 def assign_rooms(array_name)
   x = []
   array_name.each_with_index do |name,index|
-  x<< "Hello, #{name}! You'll be assigned to room #{index}"
+  x<< "Hello, #{name}! You'll be assigned to room #{index+1}"
   end 
   x
     
